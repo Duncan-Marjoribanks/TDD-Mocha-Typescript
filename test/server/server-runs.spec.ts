e9cd4ab757c748484f65d17096ff3e12f2724317
krembler <marjoribanks.d@gmail.com>
2022-06-06 20:54:11 +0100

    created basic server

--- test/server/server-runs.spec.ts
+++ test/server/server-runs.spec.ts
@@ -1,6 +1,19 @@
+import request from "supertest";
+import { expect } from "chai";
+
+import createServer from "server";
+const app = createServer();
+
+
 describe("server-checks", function() {
-    it("server is created without error", function(){
+    it("server is created without error", function() {
         console.log('hi from the server folder');
     });
-
 });
+
+describe("server-checks-GET", function() {
+    it("server should be running and return a 200 response", function(done) {
+        request(app).get("/").expect(200, done);
+    });
+});
+
