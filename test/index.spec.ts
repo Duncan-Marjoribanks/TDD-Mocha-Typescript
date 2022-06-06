f491371ba64e3ed089b9af62b845677a5a23d835
krembler <marjoribanks.d@gmail.com>
2022-06-06 20:24:06 +0100

    added index test

---
+++ test/index.spec.ts
@@ -0,0 +1,6 @@
+describe("server-checks", function() {
+    it("server is created without error", function(){
+        console.log('hi from the index file');
+    });
+
+});
--- test/server/server-runs.spec.ts
+++ test/server/server-runs.spec.ts
@@ -1,6 +1,6 @@
 describe("server-checks", function() {
     it("server is created without error", function(){
-        console.log('hi');
+        console.log('hi from the server folder');
     });
 
 });
